f7e57a854f2a23847b6c81d43ab6a5ad7676aa6f
Marco Soria <marco.soria@pucp.edu.pe>
2023-11-25 15:40:20 -0500

    routes and products controller creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/new'
+  get 'products/create'
+  get 'products/show'
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :products
 end
