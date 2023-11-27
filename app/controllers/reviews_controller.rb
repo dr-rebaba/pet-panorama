4e29a9e3ca2b6e69812bcc73bda218663a473200
Marco Soria <marco.soria@pucp.edu.pe>
2023-11-27 09:32:30 -0500

    reviews controller actions created

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,24 @@
 class ReviewsController < ApplicationController
   def create
+    @review = Review.new(review_params)
+    @product = Product.find(params[:product_id])
+    @review.product = @product
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render "producs/show", status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
   end
 end
