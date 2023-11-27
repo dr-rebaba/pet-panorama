11114d6c57c1a93e88ce32ac1b620f1d63e42db5
Marco Soria <marco.soria@pucp.edu.pe>
2023-11-27 18:57:09 -0500

    se agregaron columnas a user y annotate

--- Gemfile
+++ Gemfile
@@ -80,3 +80,5 @@ group :test do
   gem "selenium-webdriver"
 
 end
+
+gem "annotate", "~> 3.2"
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  price       :decimal(, )
+#  quantity    :integer
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_products_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
