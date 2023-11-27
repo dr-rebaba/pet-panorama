c0e45f3fa1cecff6b621e3da49dfd8c24d219688
Marco Soria <marco.soria@pucp.edu.pe>
2023-11-27 09:25:31 -0500

    product controller actions created

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,21 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :destroy]
+  before_action :set_product, only: [:show, :destroy, :edit, :update]
 
   def index
     @products = Products.all
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -19,9 +26,16 @@ class ProductsController < ApplicationController
   end
 
   def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @product.destroy
+    redirect_to products_path, status: :see_other
   end
 
   private
@@ -31,6 +45,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:list).permit(:name, :photo)
+    params.require(:list).permit(:title, :description, :quantity, :price, :photo)
   end
 end
