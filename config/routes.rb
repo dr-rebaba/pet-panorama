0b8df53dd618bd09c19b78fe44a9e4e2847e06dd
Antonio Villalobos <antoniovillalobos@gmail.com>
2023-12-02 15:56:57 -0300

    seeds ok

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
   get 'products/update'
   get 'products/destroy'
 
+  
+
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
