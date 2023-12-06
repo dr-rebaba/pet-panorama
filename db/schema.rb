9641b6381f9648378ba1b1697e3b7963c019fc4f
Leonardo Diaz <leo.diaz.trejo@gmail.com>
2023-12-05 20:16:04 -0600

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_28_011330) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_06_014513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,7 +84,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_011330) do
     t.string "title"
     t.text "description"
     t.integer "quantity"
-    t.decimal "price"
+    t.float "price"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
