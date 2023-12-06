63b1c45d43813a211fd411248fb7e2f7264eab8e
Leonardo Diaz <leo.diaz.trejo@gmail.com>
2023-12-05 18:22:25 -0600

    update

--- app/models/order.rb
+++ app/models/order.rb
@@ -16,7 +16,7 @@
 # Foreign Keys
 #
 #  fk_rails_...  (user_id => users.id)
-#
+# comentario
 class Order < ApplicationRecord
   has_many :order_products, dependent: :destroy
   has_many :products, through: :order_products
