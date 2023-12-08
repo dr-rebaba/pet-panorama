8a645f4a1ee3c380ee3ff72e5069a2a1babd0fc5
Antonio Villalobos <antoniovillalobos@gmail.com>
2023-12-07 23:02:14 -0300

    diseño home, cards, carro en progreso v.7

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,7 @@ class CartsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @cart = current_user.cart
+    @cart = current_user.cart || current_user.create_cart
     @cart_products = @cart.cart_products.includes(:product)
     @total_due = calculate_total_due(@cart_products)
   end
